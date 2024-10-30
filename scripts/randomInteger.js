468418248ff25b1e7fcc9a2df6c400541598dee2
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2024-10-30 01:01:38 +0100

    Handle linter and formatting issues

--- scripts/randomInteger.js
+++ scripts/randomInteger.js
@@ -1,4 +1,5 @@
 #! /usr/bin/env node
+/* eslint-disable no-console */
 const myArgs = process.argv.slice(2)
 let MIN = -10
 let MAX = 10
@@ -11,17 +12,15 @@ if (myArgs.length > 1) {
 	MAX = Number(myArgs[1])
 }
 
-// eslint-disable-next-line func-style
-function getRandomArbitrary(min, max) {
+const getRandomArbitrary = function getRandomArbitrary(min, max) {
 	return Math.random() * (max - min) + min
 }
 
 let number = getRandomArbitrary(MIN, MAX)
 number = Math.sign(number) * Math.round(Math.abs(number))
 
-if (Object.is(number, -0)) {
+if (Math.sign(number) === -0) {
 	number = 0
 }
 
-// eslint-disable-next-line no-console
 console.log(number)
